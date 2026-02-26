fb5517f64cc7ac8ccd62a8357cd493b08302fcc4
Dabbiru Sreeraj <sreerajscs916225@gmail.com>
2026-02-26 22:40:20 +0530

    refractor : changed rpo stucture (#4)
    Co-authored-by: sreeraj-patnaik <sreerajscs16225@gmail.com>

--- backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SimplifyReportView
-
-urlpatterns = [
-    path("simplify-report/", SimplifyReportView.as_view(), name="simplify-report"),
-]
--- backend/risk/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RiskConfig(AppConfig):
-    name = 'risk'
